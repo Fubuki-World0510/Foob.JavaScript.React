55337699637609544d0552b71ba52a3257f80ca2
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-10-12 19:29:28 +0700

    Ya, complete to learn React DOM

---
+++ src/views/Example/ChildComponent.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+class ChildComponent extends React.Component {
+    //re-render
+    state = {
+        showJobs: false
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
+
+    removed(id) {
+        this.props.rmJobs(id)
+    }
+    render() {
+        let { arrJobs } = this.props;
+        let { showJobs } = this.state;
+        let check = showJobs === true ? 'showJobs = true' : 'showJobs = false';
+        console.log('>>> check conditional: ', check)
+        return (
+            <>
+                {showJobs === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                    :
+                    <>
+                        <div className="job-lists">
+                            {
+                                arrJobs.map((item, index) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.title} - {item.salary} <><span onClick={() => this.removed(item.id)}>Xóa</span></>
+                                        </div>
+                                    )
+                                })
+
+                            }
+
+                        </div>
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                }
+            </>
+        )
+
+    }
+}
+
+// const ChildComponent = (props) => {
+
+//     let { arrJobs } = props;
+
+//     return (
+//         <>
+//             <div className="job-lists">
+//                 {
+//                     arrJobs.map((item, index) => {
+//                         if (+item.salary >= 500) {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary} $
+//                                 </div>
+//                             )
+//                         }
+
+//                     })
+
+//                 }
+
+//             </div>
+//         </>
+//     )
+// }
+export default ChildComponent;
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+import AddComponent from './AddComponent';
+
+class MyComponent extends React.Component {
+
+
+    //key:value
+    state = {
+        arrJobs: [
+            { id: 'abcJob1', title: 'Developers', salary: '500' },
+            { id: 'abcJob2', title: 'Testers', salary: '400' },
+            { id: 'abcJob3', title: 'Project managers', salary: '1000' }
+        ]
+    }
+
+
+    addNewJob = (job) => {
+        console.log('check job from parent: ', job)
+        this.setState({
+            arrJobs: [...this.state.arrJobs, job]
+        })
+
+    }
+
+    removeJob = (id) => {
+        let rm = this.state.arrJobs.filter((data) => data.id !== id)
+        console.log("REMOVED ?", rm, id)
+        this.setState({
+            arrJobs: rm
+        })
+    }
+
+
+    /* 
+    JSX => return block
+    fragment
+    */
+
+    //re-render
+    render() {
+        console.log('>>> call render: ', this.state)
+        return (
+            <>
+                <AddComponent
+                    addNewJob={this.addNewJob}
+                />
+
+
+                <ChildComponent
+                    arrJobs={this.state.arrJobs}
+                    rmJobs={this.removeJob}
+                />
+            </>
+        )
+
+    }
+}
+
+export default MyComponent;
